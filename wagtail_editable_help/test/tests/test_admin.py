ea413bd580d36553513351680e1bff7dd43b5a0a
Matt Westcott <matt@west.co.tt>
2022-09-08 16:24:36 +0100

    Add test for presence of edit link

--- wagtail_editable_help/test/tests/test_admin.py
+++ wagtail_editable_help/test/tests/test_admin.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 from django.test import TestCase
 
 try:
@@ -12,6 +12,8 @@ from wagtail_editable_help.models import HelpTextString
 class TestAdmin(TestCase):
     def setUp(self):
         self.superuser = User.objects.create_superuser("admin", "admin@example.com", "password")
+        self.normal_user = User.objects.create_user("ed", "ed@example.com", "password")
+        self.normal_user.groups.add(Group.objects.get(name="Editors"))
         self.root_page = Page.objects.get(depth=2)
 
     def test_get_default_text(self):
@@ -30,7 +32,7 @@ class TestAdmin(TestCase):
         self.assertEqual(HelpTextString.objects.count(), 1)
 
     def test_get_alternative_text(self):
-        HelpTextString.objects.create(
+        help_text = HelpTextString.objects.create(
             model_label="Home page", identifier="tagline", text="Write something clickbaity here"
         )
         self.client.login(username="admin", password="password")
@@ -42,6 +44,28 @@ class TestAdmin(TestCase):
         # Help text should pick up the custom HelpTextString record
         self.assertNotContains(response, "Write something snappy here")
         self.assertContains(response, "Write something clickbaity here")
+
+        # superuser should get the Edit link
+        self.assertContains(response, '(<a href="/admin/wagtail_editable_help/helptextstring/edit/%d/">Edit</a>)' % help_text.id)
+
+        self.assertEqual(HelpTextString.objects.count(), 1)
+
+    def test_get_alternative_text_as_normal_user(self):
+        help_text = HelpTextString.objects.create(
+            model_label="Home page", identifier="tagline", text="Write something clickbaity here"
+        )
+        self.client.login(username="ed", password="password")
+        response = self.client.get(
+            "/admin/pages/add/wagtail_editable_help_test/homepage/%d/" % self.root_page.id
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # Help text should pick up the custom HelpTextString record
+        self.assertNotContains(response, "Write something snappy here")
+        self.assertContains(response, "Write something clickbaity here")
+
+        # normal user without edit permission on HelpTextString should not get the Edit link
+        self.assertNotContains(response, '(<a href="/admin/wagtail_editable_help/helptextstring/edit/%d/">Edit</a>)' % help_text.id)
 
         self.assertEqual(HelpTextString.objects.count(), 1)
 
