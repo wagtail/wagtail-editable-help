f1115f478a85dd3c44ba499ae379a943afd432d1
Matt Westcott <matt@west.co.tt>
2023-11-03 19:28:28 +0000

    Additional settings to placate warnings

--- wagtail_editable_help/test/apps.py
+++ wagtail_editable_help/test/apps.py
@@ -5,3 +5,4 @@ class WagtailEditableHelpTestAppConfig(AppConfig):
     label = "wagtail_editable_help_test"
     name = "wagtail_editable_help.test"
     verbose_name = "Wagtail Editable Help tests"
+    default_auto_field = "django.db.models.AutoField"
